d856169174046e0b493abea98f0d2d621e5bace8
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-02-05 11:34:43 +0530

    completed my assignment

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const { resolve } = require('path');
 
+dotenv.config();
+
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3000;
+const db_user = process.env.DB_USER;
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+
+.then(() => {
+  console.log('Connected to the database');
+})
+.catch((err) => {
+  console.error(err);
+});
+
+
 
 app.use(express.static('static'));
 
